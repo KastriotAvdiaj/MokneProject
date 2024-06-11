9f27f2413d2ea0a32f1d448f232b4b4e44f4c100
Kastriot <kaloti.avdiaj@gmail.com>
2024-06-11 18:57:35 +0200

    Added the video in the about us page

--- src/Pages/About Us/AboutUs.jsx
+++ src/Pages/About Us/AboutUs.jsx
@@ -5,33 +5,51 @@ import Bottle from "../../assets/noBcgImage2.png";
 import trees from "../../assets/trees.png";
 import { InfiniteSlider } from "../../Components/InfiniteSlider/InfiniteSlider";
 import { FadeIn } from "../../Components/Reveal/FadeIn";
+import { Reveal } from "../../Components/Reveal/Reveal";
+import bottlesVid from "../../assets/compressedVid.mp4";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Divider from "../../Components/Divider/Divider";
 
 export const AboutUs = () => {
-  return (
-    <div className="about-us" style={{ backgroundImage: `url(${Montains})` }}>
-      <div
-        className="trees-overlay"
-        style={{ backgroundImage: `url(${trees})` }}
-      ></div>
-      <FadeIn position={"absolute"} top={0} right={40} y={-20}>
-        <img src={Bottle} className="about-us-bottle" />
-      </FadeIn>
-      <FadeIn position={"absolute"} top={0} right={40} y={20}>
-        <img src={Bottle} className="about-us-bottle2" />
-      </FadeIn>
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
 
-      <div className="about-us-content">
-        <h1>Rreth MOKNE</h1>
-        <p>
-          Kompania Mokne shpk është kompani e re prodhuese e cila aktivitet
-          primar e ka ujin natyral të amballazhuar.
-        </p>
-        <p>
-          Ne besojmë në njohjen dhe shpërblimin e punonjësve të jashtëzakonshëm,
-          dhe ofrojmë mundësi për rritje dhe avancim brenda kompanisë sonë.
-        </p>
+  return (
+    <div>
+      <div className="about-us-first-container">
+        <div className="about-us-first">
+          <Reveal>
+            <motion.h1 style={{ scale }}>
+              Nga bjeshkët e Istogut <br /> në tryezën tuaj
+            </motion.h1>
+          </Reveal>
+          <motion.div className="about-us-video-container" style={{ scale }}>
+            <motion.video
+              className="about-us-video"
+              src={bottlesVid}
+              autoPlay
+              loop
+              muted
+              style={{ scale }}
+            />
+          </motion.div>
+        </div>
       </div>
-      <InfiniteSlider />
+      <div className="about-us" style={{ backgroundImage: `url(${Montains})` }}>
+        <div
+          className="trees-overlay"
+          style={{ backgroundImage: `url(${trees})` }}
+        ></div>
+        <FadeIn position={"absolute"} top={0} right={40} y={-20} once={true}>
+          <img src={Bottle} className="about-us-bottle" />
+        </FadeIn>
+        <FadeIn position={"absolute"} top={0} right={40} y={20} once={true}>
+          <img src={Bottle} className="about-us-bottle2" />
+        </FadeIn>
+        <div className="infiniteSlider-container">
+          <InfiniteSlider />
+        </div>
+      </div>
     </div>
   );
 };
