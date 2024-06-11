0cd16de7057358bec1aaa48e095e0a230474976b
Kastriot <kaloti.avdiaj@gmail.com>
2024-06-11 19:44:50 +0200

    Added some more sections in the about us page.

--- src/Pages/About Us/AboutUs.jsx
+++ src/Pages/About Us/AboutUs.jsx
@@ -8,6 +8,7 @@ import { FadeIn } from "../../Components/Reveal/FadeIn";
 import { Reveal } from "../../Components/Reveal/Reveal";
 import bottlesVid from "../../assets/compressedVid.mp4";
 import { motion, useScroll, useTransform } from "framer-motion";
+import DividerWithLogo from "../../Components/Divider/DividerWithLogo";
 import Divider from "../../Components/Divider/Divider";
 
 export const AboutUs = () => {
@@ -20,19 +21,44 @@ export const AboutUs = () => {
         <div className="about-us-first">
           <Reveal>
             <motion.h1 style={{ scale }}>
-              Nga bjeshkët e Istogut <br /> në tryezën tuaj
+              Nga bjeshkët e Istogut <br /> në tryezën tuaj.
             </motion.h1>
           </Reveal>
-          <motion.div className="about-us-video-container" style={{ scale }}>
-            <motion.video
-              className="about-us-video"
-              src={bottlesVid}
-              autoPlay
-              loop
-              muted
-              style={{ scale }}
-            />
-          </motion.div>
+          <div className="under-h1-container">
+            <motion.div className="firstH1-wrapper" style={{ scale }}>
+              <DividerWithLogo />
+              <FadeIn x={-300} y={0} once={true}>
+                <h3>
+                  Kompania Mokne shpk është kompani e re prodhuese e cila
+                  aktivitet primar e ka ujin natyral të amballazhuar.
+                </h3>
+              </FadeIn>
+            </motion.div>
+            <FadeIn>
+              <motion.div
+                className="about-us-video-container"
+                style={{ scale }}
+              >
+                <motion.video
+                  className="about-us-video"
+                  src={bottlesVid}
+                  autoPlay
+                  loop
+                  muted
+                />
+              </motion.div>
+            </FadeIn>
+            <motion.div className="secondH1-wrapper" style={{ scale }}>
+              <DividerWithLogo />
+              <FadeIn x={300} y={0} once={true}>
+                <h3>
+                  Ne besojmë në njohjen dhe shpërblimin e punonjësve të
+                  jashtëzakonshëm, dhe ofrojmë mundësi për rritje dhe avancim
+                  brenda kompanisë sonë.
+                </h3>
+              </FadeIn>
+            </motion.div>
+          </div>
         </div>
       </div>
       <div className="about-us" style={{ backgroundImage: `url(${Montains})` }}>
