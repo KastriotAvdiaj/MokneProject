0f53b087d4bcf103244d8f8ed69d3d4f4c9cbdbc
Kastriot <kaloti.avdiaj@gmail.com>
2024-06-11 04:00:58 +0200

    Created an infinite slider component with empty divs, Imported it into the about us page Imported the other half of the background image to create a parallax effect on this page too.

---
+++ src/Components/InfiniteSlider/InfiniteSlider.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./InfiniteSlider.css";
+
+export const InfiniteSlider = () => {
+  return (
+    <div class="infiniteSlider-wrapper">
+      <div class="item item1"></div>
+      <div class="item item2"></div>
+      <div class="item item3"></div>
+      <div class="item item4"></div>
+      <div class="item item5"></div>
+      <div class="item item6"></div>
+      <div class="item item7"></div>
+      <div class="item item8"></div>
+    </div>
+  );
+};
--- src/Pages/About Us/AboutUs.jsx
+++ src/Pages/About Us/AboutUs.jsx
@@ -1,22 +1,37 @@
 import React from "react";
-import './AboutUs.css';
+import "./AboutUs.css";
 import Montains from "../../assets/natyre water.jpg";
-import Bottle from "../../assets/bottle.jpg";
+import Bottle from "../../assets/noBcgImage2.png";
+import trees from "../../assets/trees.png";
+import { InfiniteSlider } from "../../Components/InfiniteSlider/InfiniteSlider";
+import { FadeIn } from "../../Components/Reveal/FadeIn";
 
 export const AboutUs = () => {
   return (
-    <div 
-      className="about-us" 
-      style={{ backgroundImage: `url(${Montains})` }}
-    >
+    <div className="about-us" style={{ backgroundImage: `url(${Montains})` }}>
+      <div
+        className="trees-overlay"
+        style={{ backgroundImage: `url(${trees})` }}
+      ></div>
+      <FadeIn position={"absolute"} top={0} right={40} y={-20}>
+        <img src={Bottle} className="about-us-bottle" />
+      </FadeIn>
+      <FadeIn position={"absolute"} top={0} right={40} y={20}>
+        <img src={Bottle} className="about-us-bottle2" />
+      </FadeIn>
+
       <div className="about-us-content">
         <h1>Rreth MOKNE</h1>
-        <p>Kompania Mokne shpk është kompani e re prodhuese e cila aktivitet primar e ka ujin natyral të amballazhuar.</p>
-        <p>Ne besojmë në njohjen dhe shpërblimin e punonjësve të jashtëzakonshëm, dhe ofrojmë mundësi për rritje dhe avancim brenda kompanisë sonë.</p>
+        <p>
+          Kompania Mokne shpk është kompani e re prodhuese e cila aktivitet
+          primar e ka ujin natyral të amballazhuar.
+        </p>
+        <p>
+          Ne besojmë në njohjen dhe shpërblimin e punonjësve të jashtëzakonshëm,
+          dhe ofrojmë mundësi për rritje dhe avancim brenda kompanisë sonë.
+        </p>
       </div>
-      <div className="bottle-container">
-        <img src={Bottle} alt="Bottle" className="bottle-image" />
-      </div>
+      <InfiniteSlider />
     </div>
   );
 };
